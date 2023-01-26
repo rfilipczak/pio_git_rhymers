910254b7a1afb81f9195902520db222df04bb885
Rafal Filipczak <rfilipczak3@gmail.com>
2023-01-26 15:11:10 +0100

    2. Poprawa formatowania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,24 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo()) {
+            totalRejected++;
+        } else {
+            super.countIn(in);
+        }
+    }
 }
+
+// Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+// Odp: 5,7-9,11-17
+
+//    Zweryfikuj działanie kombinacji klawiszy alt + ← oraz alt + →. Komentarz na ten temat zamieść w
+//        ostatnio edytowanym pliku (automatyczna generacja komentarza na zaznaczeniu ctrl+/).
+// Odp: U mnie na moim IDE ta kombinacja klawiszy to przelaczanie miedzy otwartymi plikami.
