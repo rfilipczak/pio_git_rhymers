b93230bcb78197459a100016a13646b95cd37370
Rafal Filipczak <rfilipczak3@gmail.com>
2023-01-26 15:17:54 +0100

    3.1 zmiana konwencji nazewniczej klas

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
